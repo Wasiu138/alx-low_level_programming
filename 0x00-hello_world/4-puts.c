78d7a60cdc690a0f9b25bef0a9a319327d1c8722
Balogun Wasiu <wabwealthwasiu@gmail.com>
2023-09-08 01:44:30 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/** main - A C program that prints exactly with puts
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
